0af806aaf251f365f8c265fc324e109fcf9fd113
malkhaleel88 <m.alkhaleel@yahoo.com>
2021-10-04 19:07:58 +0300

    finish code challenge 7

--- challenge05/app/src/main/java/challenge05/structure/LinkedList.java
+++ challenge05/app/src/main/java/challenge05/structure/LinkedList.java
@@ -4,31 +4,38 @@ import challenge05.data.LinkedListNode;
 
 public class LinkedList {
 
-   public LinkedListNode head;
+    public LinkedListNode head;
+    int size;
+
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     *  Code Challenge 05
+     */
 
     public void insert(String data) {
         if (head == null) {
             LinkedListNode node = new LinkedListNode(data);
             head = node;
         } else {
-            LinkedListNode current = head;
-            while (current.getNext() != null) {
-                current = current.getNext();
-            }
-            LinkedListNode node = new LinkedListNode(data);
-            current.setNext(node);
+            LinkedListNode newNode = new LinkedListNode(data);
+            newNode.setNext(head);
+            head = newNode;
         }
+        size++;
     }
 
-    public boolean include(String data){
-        if(head == null){
+    public boolean include(String data) {
+        if (head == null) {
             LinkedListNode node = new LinkedListNode(data);
             head = node;
-        }else {
+        } else {
             LinkedListNode current = head;
 
-            while(current != null){
-                if(current.getData() == data){
+            while (current != null) {
+                if (current.getData() == data) {
                     return true;
                 }
                 current = current.getNext();
@@ -53,6 +60,10 @@ public class LinkedList {
         return printing;
     }
 
+    /**
+     *  Code Challenge 06
+     */
+
     public void append(String data) {
         if (head == null) {
             LinkedListNode node = new LinkedListNode(data);
@@ -65,39 +76,69 @@ public class LinkedList {
             LinkedListNode node = new LinkedListNode(data);
             current.setNext(node);
         }
+        size++;
     }
 
     public void insertBefore(String data, String before) {
         LinkedListNode node = new LinkedListNode(before);
         if (head == null) {
-            head = node;
+            System.out.println(" { " + data + " } " + " Not Exist");
+            return;
         } else if (data == head.getData()) {
             node.next = head;
             head = node;
+            size++;
         } else {
             LinkedListNode temp = head;
-            while (temp.next.getData() != data) {
+            while (temp.next != null) {
+                if (temp.next.getData() == data) {
+                    node.next = temp.next;
+                    temp.next = node;
+                    size++;
+                    return;
+                }
                 temp = temp.next;
             }
-            node.next = temp.next;
-            temp.next = node;
+            System.out.println(" { " + data + " } " + " Not Exist");
+        }
+    }
+
+    public void insertAfter(String data, String after) {
+        LinkedListNode node = new LinkedListNode(after);
+        if (head == null) {
+            System.out.println(" { " + data + " } " + " Not Exist");
+            return;
+        } else {
+            LinkedListNode temp = head;
+            while (temp != null) {
+                if (temp.getData() == data) {
+                    node.next = temp.next;
+                    temp.next = node;
+                    size++;
+                    return;
+                }
+                temp = temp.next;
+            }
+            System.out.println(" { " + data + " } " + " Not Exist");
         }
 
     }
 
-    public void insertAfter(String data , String after){
-        LinkedListNode node = new LinkedListNode(after);
-        if (head == null) {
-            head = node;
-        }else {
-            LinkedListNode temp = head;
-            while (temp.getData() != data) {
-                temp = temp.next;
-            }
-            node.next = temp.next;
-            temp.next = node;
-        }
+    /**
+     *  Code Challenge 07
+     */
 
+    public String kthFromEnd(int index)  {
+        LinkedListNode current = head;
+        if (index > getSize() - 1 || index < 0) {
+            return "Exception";
+        }
+        int i = 0;
+        while (i < (getSize() - index - 1)) {
+            current = current.getNext();
+            i++;
+        }
+        return current.getData();
     }
 
 }
