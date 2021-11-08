94eefb21d209ea96e7226f38519045db9441daee
malkhaleel88 <m.alkhaleel@yahoo.com>
2021-11-08 22:22:18 +0200

    complete code challenge 32

---
+++ challenge30/app/src/main/java/challenge30/BinaryTree.java
@@ -0,0 +1,20 @@
+package challenge30;
+
+public class BinaryTree {
+        BinaryTreeNode root;
+
+        public BinaryTree() {
+            root = null;
+        }
+
+        public BinaryTreeNode getRoot() {
+            return root;
+        }
+
+        public void setRoot(BinaryTreeNode root) {
+            this.root = root;
+        }
+
+
+
+}
